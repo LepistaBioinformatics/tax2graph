e0ede58e618fc7d2492d6c999250fd00c0c12edd
Samuel Galvão Elias <sgelias@outlook.com>
2020-07-13 11:23:45 -0300

    Include __all__ in __init__.py.

--- tax2graph/__init__.py
+++ tax2graph/__init__.py
@@ -1,3 +1,11 @@
 from .abstract_connection import ConnectionType
 from .graph_builder import GraphBuilder
 from .manager import CustomNodeType, Manager
+
+
+__all__ = [
+    'ConnectionType',
+    'GraphBuilder',
+    'CustomNodeType',
+    'Manager',
+]
